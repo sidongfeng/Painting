852d158d48ffa22e43dd0bb86fa0155f84d831f6
Sidong Charlie Feng <u6063820@anu.edu.au>
2019-08-29 22:40:13 +1000

    add file upload

--- app.js
+++ app.js
@@ -4,11 +4,44 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
+const multer = require('multer');
 
 const indexRouter = require('./routes/index');
 const testRouter = require('./routes/test');
 const app = express();
 
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+      cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+// Init Upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(req, file, cb){
+      checkFileType(file, cb);
+    }
+  }).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +54,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
-app.use('/',indexRouter);
+app.use('/index',indexRouter);
 app.use('/test',testRouter);
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+      if(err){
+        // change ejs name
+        res.render('index', {
+          msg: err
+        });
+      } else {
+        if(req.file == undefined){
+          res.render('index', {
+            msg: 'Error: No File Selected!'
+          });
+        } else {
+          res.render('index', {
+            msg: 'File Uploaded!',
+            file: `uploads/${req.file.filename}`
+          });
+        }
+      }
+    });
+  });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -41,5 +96,6 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
 
 module.exports = app;
