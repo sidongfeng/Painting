843fef727c0f9b51311884e4ddfc127f07adf59d
Sidong Charlie Feng <u6063820@anu.edu.au>
2019-09-01 05:06:22 +1000

    add custom

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -47,9 +47,7 @@ $(document).ready(function(){
 
 			// add hover and click 
 			click_hoverElement('#'+id+'header');
-
 		} 
-		
 	})
 
 	/* allow tooltip */
@@ -67,6 +65,97 @@ $(document).ready(function(){
 		});
 	});
 	/*选择图片*/
+	load_pic_click()
+	
+	//tab
+	//:eq() 选择器选取带有指定 index 值的元素。
+	//index 值从 0 开始，所有第一个元素的 index 值是 0（不是 1）
+	//siblings() 获得匹配集合中每个元素的同胞，通过选择器进行筛选是可选的。
+	$(".type li").click(function () {
+		$(".type li").eq($(this).index()).addClass("active").siblings().removeClass('active');
+		$(".pic>ul").removeClass('on').eq($(this).index()).addClass("on");
+		var firstli = $(".pic>ul").eq($(this).index()).children().first();
+		var firstimg = firstli.children("img").attr('src');
+		$(".llll").css({
+			"background": "url("+firstimg+")",
+			"background-size":"100% auto",
+			"background-position":"center center ",
+			"background-repeat":"no-repeat"
+		});
+	});
+});
+/*custom image*/
+function change() {
+	var pic = document.getElementById("preview"),
+		file = document.getElementById("f");
+
+	var ext=file.value.substring(file.value.lastIndexOf(".")+1).toLowerCase();
+
+	// gif在IE浏览器暂时无法显示
+	if(ext!='png'&&ext!='jpg'&&ext!='jpeg'){
+		alert("Image support format: jpg, png");
+		return;
+	}
+	var isIE = navigator.userAgent.match(/MSIE/)!= null,
+		isIE6 = navigator.userAgent.match(/MSIE 6.0/)!= null;
+
+	if(isIE) {
+		file.select();
+		var reallocalpath = document.selection.createRange().text;
+
+		// IE6浏览器设置img的src为本地路径可以直接显示图片
+		if (isIE6) {
+			pic.src = reallocalpath;
+		}else {
+			// 非IE6版本的IE由于安全问题直接设置img的src无法显示本地图片，但是可以通过滤镜来实现
+			pic.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='image',src=\"" + reallocalpath + "\")";
+			// 设置img的src为base64编码的透明图片 取消显示浏览器默认图片
+			pic.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
+		}
+	}else {
+		html5Reader(file);
+	}
+}
+
+function html5Reader(file){
+	var file = file.files[0];
+	var reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = function(e){
+		html = '<img src="'+this.result+'" />'
+		$(".add").append(html)
+		load_pic_click()
+		// var pic = document.getElementById("preview");
+		// pic.src=this.result;	
+	}
+}
+
+// click element in red
+// hover element in blue
+function click_hoverElement(id){
+	$(id).hover(function(){
+		$(this).css("border-style", "solid");
+		// $(this).css("border-width", "5px");
+		$(this).css("border-color", "lightblue");
+		
+		}, function(){
+			if (!$(this).hasClass('active1')) {
+				$(this).css("border-style", "none");
+			}else{
+				$(this).css("border-color", "red");
+			}
+	});
+	$(id).click(function(){
+		if ($(this).hasClass('active1')) {
+			$(this).removeClass('active1');
+		} else {
+			$(this).addClass('active1');
+			$(this).css("border-color", "red");
+		}
+	});
+}
+
+function load_pic_click(){
 	$(".pic li img").click(function(){
 		var url = $(this).attr('src');
 		img_info = img_dict[url.split('/')[2]+url.split('/')[3].split('.')[0]]
@@ -110,89 +199,6 @@ $(document).ready(function(){
 			$(ele.parentNode.parentNode).attr('data-original-title',c+': '+clientWidth+'x'+clientHeight);
 			// replace image
 			$("#"+activeNode[0].id).attr("src", url);
-		}
-	})
-	//tab
-	//:eq() 选择器选取带有指定 index 值的元素。
-	//index 值从 0 开始，所有第一个元素的 index 值是 0（不是 1）
-	//siblings() 获得匹配集合中每个元素的同胞，通过选择器进行筛选是可选的。
-	$(".type li").click(function () {
-		$(".type li").eq($(this).index()).addClass("active").siblings().removeClass('active');
-		$(".pic>ul").removeClass('on').eq($(this).index()).addClass("on");
-		var firstli = $(".pic>ul").eq($(this).index()).children().first();
-		var firstimg = firstli.children("img").attr('src');
-		$(".llll").css({
-			"background": "url("+firstimg+")",
-			"background-size":"100% auto",
-			"background-position":"center center ",
-			"background-repeat":"no-repeat"
-		});
-	});
-});
-/*custom image*/
-function change() {
-	var pic = document.getElementById("preview"),
-		file = document.getElementById("f");
-
-	var ext=file.value.substring(file.value.lastIndexOf(".")+1).toLowerCase();
-
-	// gif在IE浏览器暂时无法显示
-	if(ext!='png'&&ext!='jpg'&&ext!='jpeg'){
-		alert("图片的格式必须为png或者jpg或者jpeg格式！");
-		return;
-	}
-	var isIE = navigator.userAgent.match(/MSIE/)!= null,
-		isIE6 = navigator.userAgent.match(/MSIE 6.0/)!= null;
-
-	if(isIE) {
-		file.select();
-		var reallocalpath = document.selection.createRange().text;
-
-		// IE6浏览器设置img的src为本地路径可以直接显示图片
-		if (isIE6) {
-			pic.src = reallocalpath;
-		}else {
-			// 非IE6版本的IE由于安全问题直接设置img的src无法显示本地图片，但是可以通过滤镜来实现
-			pic.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='image',src=\"" + reallocalpath + "\")";
-			// 设置img的src为base64编码的透明图片 取消显示浏览器默认图片
-			pic.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
-		}
-	}else {
-		html5Reader(file);
-	}
-}
-
-function html5Reader(file){
-	var file = file.files[0];
-	var reader = new FileReader();
-	reader.readAsDataURL(file);
-	reader.onload = function(e){
-		var pic = document.getElementById("preview");
-		pic.src=this.result;
-	}
-}
-
-// click element in red
-// hover element in blue
-function click_hoverElement(id){
-	$(id).hover(function(){
-		$(this).css("border-style", "solid");
-		// $(this).css("border-width", "5px");
-		$(this).css("border-color", "lightblue");
-		
-		}, function(){
-			if (!$(this).hasClass('active1')) {
-				$(this).css("border-style", "none");
-			}else{
-				$(this).css("border-color", "red");
-			}
-	});
-	$(id).click(function(){
-		if ($(this).hasClass('active1')) {
-			$(this).removeClass('active1');
-		} else {
-			$(this).addClass('active1');
-			$(this).css("border-color", "red");
-		}
+		};
 	});
 }
